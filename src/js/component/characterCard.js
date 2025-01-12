6a456a528dfe957918364dd0350012b4a8dbcb1e
frontdev1987 <119827017+frontdev1987@users.noreply.github.com>
2025-01-12 05:03:43 +0000

    still working

--- src/js/component/characterCard.js
+++ src/js/component/characterCard.js
@@ -1,9 +1,49 @@
-import React, {useState,useEffect,useContext} from "react";
-import {Link}from "react-router-dom";
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
 
-import{Context}from "../store/appContext";
+import { Context } from "../store/appContext";
 
+export const CharacterCard = () => {
+  const { store, actions } = useContext(Context);
+  const [characters, setCharacters] = useState([]);
 
-export const CharacterCard = () =>{
-    const{store,actions}=useContext(Context);
-    const[characters,setCharacters]=useState([])};
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("https://swapi.dev/api/people/");
+      const data = await res.json();
+      setCharacters(data.results);
+    }
+    fetchData();
+  }, []);
+
+  function handleFavorites(name) {
+    store.favorites.includes(name)
+      ? actions.removeFromFavorites(name)
+      : actions.addToFavorites(name);
+  }
+  return (
+    <div className="container d-flex col-10 overflow-auto mt-5 mx-auto">
+      {characters?.map((character, index) =>(
+      <div className="card" style={{ minWidth: "200px" }} key={index}>
+        {console.log(characters.name)}
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title text-dark">{character.name}</h5>
+          <button
+            className="btn btn-primary"
+            onClick={() => handleFavorites(character.name)}
+          >
+            <i className="far fa-heart"></i>
+          </button>
+          <Link
+            to={"characterDescription/" + (index + 1)}
+            className="btn btn-primary"
+          >
+            Learn More
+          </Link>
+        </div>
+      </div>
+      ))}
+    </div>
+  );
+};
