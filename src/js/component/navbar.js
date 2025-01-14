b8d397b938631430d1857df897d9aa4f25a418d5
frontdev1987 <119827017+frontdev1987@users.noreply.github.com>
2025-01-14 05:02:08 +0000

    unfinished

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-danger mb-3">
+      <i className="fa-solid fa-jedi" style={{ color: "#51291f" }}></i>
+      <Link to="/">
+        <span className="navbar-brand mb-0 h1">Home</span>
+      </Link>
+      <div className="ml-auto">
+        <div className="dropdown">
+          <btn
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            favorites
+          </btn>
+          <ul className="dropdown-menu dropdown-menu-dark mr-2 dropdon-menu-end">
+            {favorites.map((favorite, index) => (
+              <li>
+                <a className="dropdown-item" key={index}>
+                  {favorite}
+                </a>
+                <btn onClick={() => actions.removeFromFavorites(favorite)}>
+                  X
+                </btn>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
